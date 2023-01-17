d17c0be89052aaed1912ab3869bb6f3cd5630b7c
Paulo Rodrigues <paulo@rodrigues.pw>
2023-01-17 19:43:56 -0300

    generator do projeto e remoção de assets não utilizados

---
+++ API.Tests/Controllers/ValuesControllerTest.cs
@@ -0,0 +1,81 @@
+﻿using API;
+using API.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+
+namespace API.Tests.Controllers
+{
+    [TestClass]
+    public class ValuesControllerTest
+    {
+        [TestMethod]
+        public void Get()
+        {
+            // Arrange
+            ProductsController controller = new ProductsController();
+
+            // Act
+            IEnumerable<string> result = controller.Get();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Count());
+            Assert.AreEqual("value1", result.ElementAt(0));
+            Assert.AreEqual("value2", result.ElementAt(1));
+        }
+
+        [TestMethod]
+        public void GetById()
+        {
+            // Arrange
+            ProductsController controller = new ProductsController();
+
+            // Act
+            string result = controller.Get(5);
+
+            // Assert
+            Assert.AreEqual("value", result);
+        }
+
+        [TestMethod]
+        public void Post()
+        {
+            // Arrange
+            ProductsController controller = new ProductsController();
+
+            // Act
+            controller.Post("value");
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void Put()
+        {
+            // Arrange
+            ProductsController controller = new ProductsController();
+
+            // Act
+            controller.Put(5, "value");
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void Delete()
+        {
+            // Arrange
+            ProductsController controller = new ProductsController();
+
+            // Act
+            controller.Delete(5);
+
+            // Assert
+        }
+    }
+}
