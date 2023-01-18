99fa31411c2359f72fca7b0f474da3c9e63a317a
Paulo Rodrigues <paulo@rodrigues.pw>
2023-01-18 00:23:29 -0300

    Criar contexto de banco e inicialização de dados de teste

---
+++ API/Data/ApiContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using API.Models;
+
+namespace API.Data
+{
+    public class ApiContext : DbContext
+    {
+        public ApiContext() : base("name=APIContext")
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ API/Data/ApiInitializer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using API.Models;
+
+namespace API.Data
+{
+    public class ApiInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<ApiContext>
+    {
+        protected override void Seed(ApiContext context)
+        {
+            var products = new List<Product>
+            {
+                new Product {CreatedAt = DateTime.Now, Name="Incredible Plastic Pants", Price=827, Brand="Hauck - Johnson", UpdatedAt=DateTime.Now },
+                new Product {CreatedAt = DateTime.Now, Name="Electronic Wooden Tuna", Price=765, Brand="Johns - Farrell", UpdatedAt=DateTime.Now },
+                new Product {CreatedAt = DateTime.Now, Name="Awesome Steel Mouse", Price=143, Brand="Paucek, Kuvalis and Zieme", UpdatedAt=DateTime.Now },
+
+            };
+
+            products.ForEach(p => { context.Products.Add(p); });
+            context.SaveChanges();
+        }
+
+    }
+}
+
